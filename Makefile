45d3dc07b40f902bb3365da477d3e4aefccb11ea
mountainmoon <yuekec@gmail.com>
2015-07-06 14:57:38 +0800

    update test

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 BINS = ./node_modules/.bin
 DUO = $(BINS)/duo
 UGLIFYJS = $(BINS)/uglifyjs
+SRC = $(wildcard lib/*.js)
 
-track.js:
+track.js: $(SRC)
 	@$(DUO) --stdout --standalone track lib/index.js > $@
+	@cp $@ test/$@
 
 track.min.js: track.js
 	@$(UGLIFYJS) $< --output $@
--- express.js
+++ express.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var cookieParser = require('cookie-parser')
 var app = express();
+
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -7,10 +10,25 @@ app.get('/', function (req, res) {
 
 // accept POST request on the homepage
 app.post('/', function (req, res) {
-    switch(req.hostname) {
-        
-    }
+    res.send('ok');
 });
+
+app.post('/track', handler);
+app.post('/page', handler);
+app.post('/identify', handler);
+
+function handler(req, res) {
+    console.log(req.hostname);
+    if (req.hostname == 'tracking.hcd.com') {
+        console.log('req.cookies: ', req.cookies);
+        console.log('req.url:', req.url);
+    }
+    req.setEncoding('utf8');
+    req.on('data', function (trunck) {
+        console.log('data:', trunck);
+    });
+    res.send('ok');
+}
 
 app.use(express.static('test'));
 
